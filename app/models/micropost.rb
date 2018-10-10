16b90a67e7adac551df13b42b46199fd8d64f32e
kate <katetechtech@gmail.com>
2018-10-10 12:04:05 +0000

    like3

---
+++ app/models/micropost.rb
@@ -0,0 +1,8 @@
+class Micropost < ApplicationRecord
+  belongs_to :user
+  
+  validates :content, presence: true, length: { maximum: 255 }
+  
+  has_many :reverses_of_like, class_name: 'Like', foreign_key: 'micropost_id'
+  has_many :like_users, through: :reverses_of_like, source: :user
+end
